d76b500407d472f57c358cbab589fc832c49c5ba
krystyna-lyn <67439674+krystyna-lyn@users.noreply.github.com>
2025-06-01 22:59:34 +0100

    update

--- src/App.js
+++ src/App.js
@@ -1,14 +1,34 @@
+import React, { useState } from 'react';
 import './index.scss';
 
 function App() {
+
+  const [open, setOpen] = useState(false);
+
+  const openModal = () => {
+    setOpen(true)
+    console.log(open)
+  }
+  const closeModal = () => {
+    setOpen(false)
+  }
+
   return (
     <div className="App">
-      <div>
-        <h2>Счетчик:</h2>
-        <h1>0</h1>
-        <button className="minus">- Минус</button>
-        <button className="plus">Плюс +</button>
-      </div>
+      <button className="open-modal-btn" onClick={openModal}>✨ open window</button>
+
+      {(open) &&
+        <div className="overlay">
+          <div className="modal">
+            <svg height="200" viewBox="0 0 200 200" width="200" onClick={closeModal}>
+              <title />
+              <path d="M114,100l49-49a9.9,9.9,0,0,0-14-14L100,86,51,37A9.9,9.9,0,0,0,37,51l49,49L37,149a9.9,9.9,0,0,0,14,14l49-49,49,49a9.9,9.9,0,0,0,14-14Z" />
+
+            </svg>
+            <img src="https://media2.giphy.com/media/xT0xeJpnrWC4XWblEk/giphy.gif" />
+          </div>
+        </div>
+      }
     </div>
   );
 }
