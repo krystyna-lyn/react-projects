f9f86ee0f1ebf4cf8b1b17a375ff3c599cb151c1
krystyna-lyn <67439674+krystyna-lyn@users.noreply.github.com>
2025-06-05 20:07:35 +0100

    add new components

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import './index.scss';
-import Users from './components/Users';
+import { Users } from './components/Users/';
 
 // https://reqres.in/api/users
 
