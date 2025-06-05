6e579e8ee3afaf4a02db4bf56e3e2ab75323bb41
krystyna-lyn <67439674+krystyna-lyn@users.noreply.github.com>
2025-06-05 19:49:14 +0100

    update

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './index.scss';
 import Users from './components/Users';
 
 // https://reqres.in/api/users
 
 function App() {
+
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    fetch('https://reqres.in/api/users')
+      .then(res => res.json())
+      .then((json) => {
+        setUsers(json.data)
+      })
+  })
+
   return (
     <div className="App">
       <Users />
