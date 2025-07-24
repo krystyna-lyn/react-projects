936497ffcacf7508b0887efbd449f3d24fd73aaa
krystyna-lyn <67439674+krystyna-lyn@users.noreply.github.com>
2025-07-24 22:18:28 +0100

    update

--- src/App.js
+++ src/App.js
@@ -7,65 +7,7 @@ import { Success } from './components/Success.jsx'
 
 function App() {
 
-  const [users, setUsers] = useState([]);
-  const [isLoading, setIsLoading] = useState(true);
-  const [search, setSearch] = useState('');
-  const [invites, setInvites] = useState([]);
-  const [success, setSuccess] = useState(false);
-  //console.log(invites)
 
-  useEffect(() => {
-    fetch('https://reqres.in/api/users', {
-      headers: {
-        'x-api-key': 'reqres-free-v1',
-      },
-    })
-      .then(res => res.json())
-      .then((json) => {
-        setUsers(json.data)
-      }).catch((err) => {
-        console.warn(err)
-        alert('Error')
-      }).finally(() => {
-        setIsLoading(false)
-      })
-  }, [])
-
-  const onchangeSearchValue = (event) => {
-    setSearch(event.target.value);
-  }
-
-  const onClickInvite = (id) => {
-    if (invites.includes(id)) {
-      setInvites(prev => prev.filter(_id => _id != id))
-    } else {
-      setInvites(prev => [...prev, id])
-    }
-  }
-
-  const onClickSuccess = () => {
-    setSuccess(true);
-  }
-
-  return (
-    <div className="App">
-      {success ? (<Success count={invites.length} />) :
-        (
-          <Users
-            search={search}
-            onchangeSearchValue={onchangeSearchValue}
-            onClickSuccess={onClickSuccess}
-            isLoading={isLoading}
-            items={users}
-            invites={invites}
-            onClickInvite={onClickInvite}
-          />
-        )
-      }
-
-
-    </div>
-  );
 }
 
 export default App;
