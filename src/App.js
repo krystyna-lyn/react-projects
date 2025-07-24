8bb7e1da1e20b622604056e3c364216019e0b18d
krystyna-lyn <67439674+krystyna-lyn@users.noreply.github.com>
2025-07-24 22:49:13 +0100

    clear

--- src/App.js
+++ src/App.js
@@ -1,17 +1,11 @@
-import React, { useEffect, useState } from 'react';
 import './index.scss';
-import { Users } from './components/Users/';
-import { Success } from './components/Success.jsx'
 
-// https://reqres.in/api/users
 
 function App() {
 
 
   return (
     <div className="App">
-
-      Converter
 
 
     </div>
