300b0c0314c5cc99c1ca9a90ed539369254b76de
krystyna-lyn <67439674+krystyna-lyn@users.noreply.github.com>
2025-08-23 19:58:19 +0100

    update Block

--- src/Block.js
+++ src/Block.js
@@ -1,8 +1,18 @@
-import React from 'react'
-
 const Block = () => {
     return (
-        <div>Block</div>
+        <div className="block">
+            <ul className="currencies">
+
+                <svg height="50px" viewBox="0 0 50 50" width="50px">
+                    <rect fill="none" height="50" width="50" />
+                    <polygon points="47.25,15 45.164,12.914 25,33.078 4.836,12.914 2.75,15 25,37.25 " />
+                </svg>
+
+            </ul>
+            <input
+                type="number"
+            />
+        </div>
     )
 }
 
